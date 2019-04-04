5c06401d231411a12da0c6832a2dea747c2e027a
Sean Hoyt <seanhoyt963@gmail.com>
2019-04-04 08:37:45 -0500

    Setup Basic file Structure and Implement a drawer
    Using this drawer library
    https://github.com/shrikanth7698/Custom-Navigation-Drawer

---
+++ app/src/main/java/com/deadman/databitsfuture/Drawer.java
@@ -0,0 +1,119 @@
+package com.deadman.databitsfuture;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+
+import android.os.Bundle;
+
+import com.shrikanthravi.customnavigationdrawer2.data.MenuItem;
+import com.shrikanthravi.customnavigationdrawer2.widget.SNavigationDrawer;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Drawer extends AppCompatActivity {
+
+    SNavigationDrawer sNavigationDrawer;
+    Class fragmentClass;
+    public static Fragment fragment;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.drawer);
+
+        sNavigationDrawer = findViewById(R.id.navigationDrawer);
+
+        // Creating a list of menu Items
+        List<MenuItem> menuItems = new ArrayList<>();
+        menuItems.add(new MenuItem("Welcome",R.drawable.news_bg));
+        menuItems.add(new MenuItem("Crowd",R.drawable.feed_bg));
+        menuItems.add(new MenuItem("Pit",R.drawable.message_bg));
+        menuItems.add(new MenuItem("Master",R.drawable.music_bg));
+
+        // Add them to navigation drawer
+
+        sNavigationDrawer.setMenuItemList(menuItems);
+        fragmentClass =  Welcome.class;
+        try {
+            fragment = (Fragment) fragmentClass.newInstance();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        if (fragment != null) {
+            FragmentManager fragmentManager = getSupportFragmentManager();
+            fragmentManager.beginTransaction().setCustomAnimations(android.R.animator.fade_in, android.R.animator.fade_out).replace(R.id.frameLayout, fragment).commit();
+        }
+
+        // Listener to handle the menu item click. It returns the position of the menu item clicked. Based on that you can switch between the fragments.
+
+        sNavigationDrawer.setOnMenuItemClickListener(new SNavigationDrawer.OnMenuItemClickListener() {
+            @Override
+            public void onMenuItemClicked(int position) {
+                System.out.println("Position "+position);
+
+                switch (position){
+                    case 0:{
+                        fragmentClass = Welcome.class;
+                        break;
+                    }
+                    case 1:{
+                        fragmentClass = Crowd.class;
+                        break;
+                    }
+                    case 2:{
+                        fragmentClass = Pit.class;
+                        break;
+                    }
+                    case 3:{
+                        fragmentClass = Master.class;
+                        break;
+                    }
+
+                }
+
+                // Listener for drawer events such as opening and closing.
+                sNavigationDrawer.setDrawerListener(new SNavigationDrawer.DrawerListener() {
+
+                    @Override
+                    public void onDrawerOpened() {
+
+                    }
+
+                    @Override
+                    public void onDrawerOpening(){
+
+                    }
+
+                    @Override
+                    public void onDrawerClosing(){
+                        System.out.println("Drawer closed");
+
+                        try {
+                            fragment = (Fragment) fragmentClass.newInstance();
+                        } catch (Exception e) {
+                            e.printStackTrace();
+                        }
+
+                        if (fragment != null) {
+                            FragmentManager fragmentManager = getSupportFragmentManager();
+                            fragmentManager.beginTransaction().setCustomAnimations(android.R.animator.fade_in, android.R.animator.fade_out).replace(R.id.frameLayout, fragment).commit();
+
+                        }
+                    }
+
+                    @Override
+                    public void onDrawerClosed() {
+
+                    }
+
+                    @Override
+                    public void onDrawerStateChanged(int newState) {
+                        System.out.println("State "+newState);
+                    }
+                });
+            }
+        });
+    }
+}
---
+++ app/src/main/java/com/deadman/databitsfuture/Welcome.java
@@ -0,0 +1,27 @@
+package com.deadman.databitsfuture;
+
+import android.os.Bundle;
+
+import androidx.annotation.NonNull;
+import androidx.fragment.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+
+public class Welcome extends Fragment {
+
+
+    public Welcome() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.welcome, container, false);
+    }
+
+}
