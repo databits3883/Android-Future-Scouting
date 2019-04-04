823ca923aaa7b4bf77651e5853e4efb544a30a19
Sean Hoyt <seanhoyt963@gmail.com>
2019-04-04 10:34:43 -0500

    Add a basic welcome page
    3883 banner, Text explaining how to open the drawer, position selector

---
+++ app/src/main/java/com/deadman/databitsfuture/CustomOnItemSelectedListener.java
@@ -0,0 +1,17 @@
+package com.deadman.databitsfuture;
+
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.AdapterView.OnItemSelectedListener;
+
+public class CustomOnItemSelectedListener implements OnItemSelectedListener {
+
+    public void onItemSelected(AdapterView<?> parent, View view, int pos,long id) {
+    }
+
+    @Override
+    public void onNothingSelected(AdapterView<?> arg0) {
+        // Auto-generated method stub
+    }
+
+}
--- app/src/main/java/com/deadman/databitsfuture/Welcome.java
+++ app/src/main/java/com/deadman/databitsfuture/Welcome.java
@@ -7,10 +7,13 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
 
+import java.util.ArrayList;
+import java.util.List;
 
 public class Welcome extends Fragment {
-
 
     public Welcome() {
         // Required empty public constructor
@@ -20,8 +23,28 @@ public class Welcome extends Fragment {
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.welcome, container, false);
+        View rootview = inflater.inflate(R.layout.welcome, container, false);
+
+        Spinner spinner1 = rootview.findViewById(R.id.spinner1);
+        List<String> list = new ArrayList<>();
+        list.add("Practice Mode");
+        list.add("Red 1");
+        list.add("Red 2");
+        list.add("Red 3");
+        list.add("Blue 1");
+        list.add("Blue 2");
+        list.add("Blue 3");
+        ArrayAdapter dataAdapter = new ArrayAdapter<>(getContext(),
+                android.R.layout.simple_spinner_item, list);
+        dataAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spinner1.setAdapter(dataAdapter);
+
+        spinner1.setOnItemSelectedListener(new CustomOnItemSelectedListener());
+
+        spinner1.setSelection(0);
+
+
+        return rootview;
     }
 
 }
